4ba4a2fca2432623b017918b6c61de8950afb38c
AuraKai <wehttam1989@gmail.com>
2025-08-02 13:25:48 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    // Android plugins with explicit versions
-    id("com.android.application") version "8.12.0" apply false
-    id("com.android.library") version "8.12.0" apply false
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
-    id("com.google.dagger.hilt.android") version "2.57" apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
+
 
 // Configure Java toolchain for all subprojects
 subprojects {
